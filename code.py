4642d662411fe417654580f0327e28ce2ee892c6
ritiek <ritiekmalhotra123@gmail.com>
2017-08-09 22:01:09 +0530

    Load credentials from .pass file

--- code.py
+++ code.py
@@ -1,7 +1,9 @@
 import requests
 
-username = 'username'
-password = 'password'
+with open('.pass', 'r') as credits:
+    decode_credits = credits.readlines()
+    username = decode_credits[0].strip('\n')
+    password = decode_credits[1].strip('\n')
 
 url = 'http://aoczone.net/ucp.php?mode=login'
 
@@ -12,3 +14,4 @@ headers = {
 data = 'username=' + username + '&password=' + password + '%40123&login=Login'
 
 login = requests.post(url, data=data, headers=headers)
+print(login.text)
