2e6bf66f87d1900084893d586fd3d22dd4d254c8
ritiek <ritiekmalhotra123@gmail.com>
2017-08-10 19:32:22 +0530

    Clean code

--- code.py
+++ code.py
@@ -1,17 +1,26 @@
 import requests
 
-with open('.pass', 'r') as credits:
-    decode_credits = credits.readlines()
+def read_credentials(passfile):
+    with open(passfile, 'r') as credits:
+        decode_credits = credits.readlines()
     username = decode_credits[0].strip('\n')
     password = decode_credits[1].strip('\n')
+    return username, password
 
-url = 'http://aoczone.net/ucp.php?mode=login'
+def aoczone_login(username, password)
+    url = 'http://aoczone.net/ucp.php?mode=login'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = 'username=' + username + '&password=' + password + '%40123&login=Login'
+    login = requests.post(url, data=data, headers=headers)
+    return login.cookies
 
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
+def post_record():
+    pass
 
-data = 'username=' + username + '&password=' + password + '%40123&login=Login'
+def rename_record():
+    pass
 
-login = requests.post(url, data=data, headers=headers)
-print(login.text)
+
+username, password = read_credentials(passfile='.pass')
+cookies = aoczone_login(username, password)
+print(cookies)
